71bdb1fab41fdea5157762aeb79d0eeb841f1f26
Félix Sánchez-Tembleque <felixstdp@gmail.com>
2022-03-22 12:42:58 +0100

    Update universal.ino

--- ArduinoUNO/universal.ino
+++ ArduinoUNO/universal.ino
@@ -47,3 +47,17 @@ void drive(int L, int R, int t) //velocidad de las ruedas izquierda y derecha, p
   
   delay(t);
 }
+
+//función de movimiento de motores
+void drive(int L, int R, int t) //velocidad de las ruedas izquierda y derecha, positivo hacia delante, tiempo en milisegundos
+{
+  L=constrain(L,-255,255); // evitar valores no válidos para el PWM
+  R=constrain(R,-255,255);
+
+  digitalWrite(AIN1, L<0);
+  analogWrite(AIN2, L+255*(L<0));
+  digitalWrite(BIN1, R<0);
+  analogWrite(BIN2, R+255*(R<0));  
+  
+  delay(t);
+}
