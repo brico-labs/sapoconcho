4848322cb964bb5e49a9dc64ae3820bb9a764faf
Félix Sánchez-Tembleque <felixstdp@gmail.com>
2022-03-23 22:00:25 +0100

    Update us_read.ino

--- ArduinoUNO/us_read.ino
+++ ArduinoUNO/us_read.ino
@@ -1,25 +1,27 @@
-// Configuración:
-void setup(){
+// Configuración
+void setup()
+{
   Serial.begin(9600);
 }
 
-// Programa:
-void loop(){
+// Programa común
+void loop()
+{
   unsigned long distancia = sonar(2);    // invocamos a función de medir distancia
-  Serial.print("Distancia: ");  // imprimimos os valores na consola
+  Serial.print("Distancia: ");  // imprimimos los valores en la consola
   Serial.print(distancia);
   Serial.println(" cm");
-
 }
 
 // Función
-unsigned long sonar(int trig){
+unsigned long sonar(int trig)
+{
   pinMode(trig, OUTPUT);
   digitalWrite(trig,LOW); // trigger envia un pulso ultrasónico
   delayMicroseconds(5);
   digitalWrite(trig, HIGH);     
   delayMicroseconds(10);
-    pinMode(trig, INPUT);
+  pinMode(trig, INPUT);
   unsigned long tiempo=pulseIn(trig, HIGH,3000);    // echo espera a recibir la respuesta
   unsigned long distancia= int(0.017*tiempo);  // fórmula para calcular la distancia
   delay(200);
